48c9c968a7fdcbe07b273997ea77a05dde8b683f
kest-3r <kest3r.05@gmail.com>
2024-11-07 19:44:25 +0800

    Viva1 Q1

--- src/Viva1/Viva1Q5.java
+++ src/Viva1/Viva1Q5.java
@@ -1,4 +1,30 @@
 package Viva1;
-
+import java.util.Scanner;
 public class Viva1Q5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] stringArray = input.split("REMIX");
+        String sentence = "";
+        boolean firstWord=true;
+
+        for(int i=0;i<stringArray.length;i++){
+            if(!(stringArray[i].isEmpty())){
+                sentence = sentence + stringArray[i];
+                if (!firstWord){
+                    sentence = sentence + " ";
+                }
+                firstWord=false;
+            }
+        }
+
+        System.out.println(sentence);
+
+
+
+
+
+    }
 }
